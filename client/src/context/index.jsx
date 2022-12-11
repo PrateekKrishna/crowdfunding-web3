8dc847caa77b517e0c87a39317bde7773bbf862d
Prateek Krishna <prateekkrishna09@gmail.com>
2022-12-12 02:59:04 +0530

    added function to get campaigns from blockchain

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -15,6 +15,7 @@ export const StateContextProvider = ({ children }) => {
   const address = useAddress();
   const connect = useMetamask();
 
+  //publish campaign in blockchain
   const publishCampaign = async (form) => {
     try {
       const data = await createCampaign([
@@ -32,6 +33,22 @@ export const StateContextProvider = ({ children }) => {
     }
   }
 
+  //get campaigns from blockchain
+  const getCampaigns = async ()=>{
+    const campaigns = await contract.call('getCampaigns');
+    const parsedCampaigns = campaigns.map((campaign, i)=>({
+      owner: campaign.owner,
+      title: campaign.title,
+      description: campaign.description,
+      target: ethers.utils.formatEther(campaign.target.toString()),
+      deadline: campaign.deadline.toNumber(),
+      amountCollected: ethers.utils.formatEther(campaign.amountCollected.toString()),
+      image: campaign.image,
+      pId: i
+
+    }))
+    return parsedCampaigns;
+  }
 
 
   return (
@@ -41,6 +58,7 @@ export const StateContextProvider = ({ children }) => {
         contract,
         connect,
         createCampaign: publishCampaign,
+        getCampaigns
       }}
     >
       {children}
